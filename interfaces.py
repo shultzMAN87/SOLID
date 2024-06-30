584af64fbbfa2936b9a4388fa122ac12ff1375af
Константин <zuker_gta@mail.ru>
2024-06-30 19:58:04 +1000

    fouth

---
+++ interfaces.py
@@ -0,0 +1,40 @@
+from abc import ABC, abstractmethod
+
+
+class PrinterInterface(ABC):
+
+    @abstractmethod
+    def printing(self):
+        pass
+
+    @abstractmethod
+    def scaning(self):
+        pass
+
+    @abstractmethod
+    def coping(self):
+        pass
+
+class Printer(PrinterInterface):
+
+    def printing(self):
+        print('Я печатаю')
+    def scaning(self):
+        raise NotImplemented('Метод не реализован в данном классе')
+
+    def coping(self):
+        raise NotImplemented('Метод не реализован в данном классе')
+
+class Scanner(PrinterInterface):
+
+    def printing(self):
+        raise NotImplemented('Метод не реализован в данном классе')
+    def scaning(self):
+        print('Я сканирую')
+
+    def coping(self):
+        raise NotImplemented('Метод не реализован в данном классе')
+
+
+printer = Printer()
+scanner = Scanner()
