c60fb8b1463b521aec08818d3dbbefb1056df25f
Pavlyushkevich.KE <zuker_gta@mail.ru>
2024-07-20 14:46:51 +1000

    fouth//*

--- DependingInv.py
+++ DependingInv.py
@@ -3,6 +3,7 @@ import random
 import json
 import xml.etree.ElementTree as ET
 
+
 class DataFromDB:
     def __init__(self, waybill_id):
         self.waybill_id = waybill_id
@@ -12,9 +13,11 @@ class DataFromDB:
                 'waybill_num': f'{random.randint(101, 999)}/{random.randint(11, 99)}',
                 'waybill_date': datetime.datetime.now().strftime('%Y-%m-%d')}
 
+
 class DataSerializator:
     def serializator(self, data):
         return json.dumps(data, indent=4)
+
 
 class DataSerializatorXML:
     def write_dict_to_xml(self, data):
@@ -27,6 +30,7 @@ class DataSerializatorXML:
         xml_str = ET.tostring(root, encoding="utf-8", method="xml")
         return xml_str.decode(encoding="utf-8")
 
+
 class WaybillCreator:
     def __init__(self, waybill_id):
         self.waybill_data = DataFromDB(waybill_id)
@@ -34,6 +38,7 @@ class WaybillCreator:
 
     def serialization_processor(self):
         print(self.waybill_serializator.serializator(self.waybill_data.data_from_db()))
+
 
 wc = WaybillCreator('52115-855-855')
 wc.serialization_processor()
--- DependingInvNew.py
+++ DependingInvNew.py
@@ -7,13 +7,15 @@ from abc import ABC, abstractmethod
 
 class SerializationService(ABC):
     @abstractmethod
-    def serializator(self, data):
+    def serializator(self, data) -> str:
         pass
+
 
 class DataSerializatorJSON(SerializationService):
     def serializator(self, data):
         return json.dumps(data, indent=4)
 
+ 
 class DataSerializatorXML(SerializationService):
     def serializator(self, data):
         root = ET.Element('root')
@@ -25,14 +27,17 @@ class DataSerializatorXML(SerializationService):
         xml_str = ET.tostring(root, encoding="utf-8", method="xml")
         return xml_str.decode(encoding="utf-8")
 
+
 class DataFromDB:
     def __init__(self, waybill_id):
         self.waybill_id = waybill_id
 
     def data_from_db(self):
-        return {'waybill_id': self.waybill_id,
-                'waybill_num': f'{random.randint(101, 999)}/{random.randint(11, 99)}',
-                'waybill_date': datetime.datetime.now().strftime('%Y-%m-%d')}
+        # return {'waybill_id': self.waybill_id,
+        #         'waybill_num': f'{random.randint(101, 999)}/{random.randint(11, 99)}',
+        #         'waybill_date': datetime.datetime.now().strftime('%Y-%m-%d')}
+        return ['1', 8, 8]
+
 
 class WaybillCreator:
     def __init__(self, waybill_id, serializator_service: SerializationService):
@@ -42,10 +47,11 @@ class WaybillCreator:
     def serialization_processor(self):
         print(self.waybill_serializator.serializator(self.waybill_data.data_from_db()))
 
+
 json_serializator = DataSerializatorJSON()
 wc = WaybillCreator('52115-855-855', json_serializator)
 wc.serialization_processor()
 
-xml_serializator = DataSerializatorXML()
-wc = WaybillCreator('32541-845-855', xml_serializator)
-wc.serialization_processor()
+# xml_serializator = DataSerializatorXML()
+# wc = WaybillCreator('32541-845-855', xml_serializator)
+# wc.serialization_processor()
